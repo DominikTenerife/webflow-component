b321f8b0b63f5bbada1268879856e76cfd6a49de
DominikTenerife <dpiskorski1@gmail.com>
2024-10-22 16:51:08 +0100

    removing images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from 'https://brave-tree-09c497e03.5.azurestaticapps.net/assets/react-CHdo91hT.svg'
-import viteLogo from 'https://brave-tree-09c497e03.5.azurestaticapps.net/vite.svg'
+
 import './App.css'
 
 
@@ -11,12 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
