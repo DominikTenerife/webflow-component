73e89aa8a0008761ebf361d3a79a607dcf69a362
DominikTenerife <dpiskorski1@gmail.com>
2024-10-22 18:49:19 +0100

    adding image

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,25 @@ import './App.css'
 
 
 
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        
-      </div>
-      <h1>Vite + React</h1>
+      
+      <h1>This is Dominik's Vite + React app</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={"https://myarchipelago.es/wp-content/uploads/2024/09/IMG_1007-768x1024.jpg"} alt="photo of a cliff in Tenerife" className="image-large"/>
+      
     </>
   )
 }
